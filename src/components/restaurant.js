492158d8655456e62b45770a833f99ba2d52e38e
HOME64\admin <elistratov-av@yandex.ru>
2019-05-20 00:23:57 +0300

    avg rating

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,14 +1,26 @@
 import React, { PureComponent } from "react";
 import RestaurantMenu from "./restaurant-menu";
+import { Rate } from "antd";
+
+function roundHalf(v) {
+  const rest = v % 1;
+  return rest >= 0.5 ? (v ^ 0) + 0.5 : v ^ 0;
+}
 
 class Restaurant extends PureComponent {
   render() {
-    const { image, name, menu, isMenuOpen } = this.props;
+    const { image, name, menu, isMenuOpen, reviews } = this.props;
+    var avgRating = reviews.length
+      ? (reviews.reduce((p, c) => p + c.rating, 0) / reviews.length).toFixed(1)
+      : 0;
 
     return (
       <div>
         <img src={image} width={64} height={64} alt={name} />
-        <h3>{name}</h3>
+        <h3>
+          {name} <Rate disabled allowHalf defaultValue={roundHalf(avgRating)} />{" "}
+          {avgRating}
+        </h3>
         <button onClick={this.handleToggleOpenClick}>
           {isMenuOpen ? "Close menu" : "Open menu"}
         </button>
